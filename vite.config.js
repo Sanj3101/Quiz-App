364a969a00198e1fa4a46b4c5ef1bf64eb8c883b
Sanjana BIswas <157401970+Sanj3101@users.noreply.github.com>
2024-12-15 03:02:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+//https://vite.dev/config/
 
 export default defineConfig({
-  esbuild: {
-    target: 'esnext', // Ensures modern JavaScript features are supported
-  }
-});
+  plugins: [react()],
+})
+
